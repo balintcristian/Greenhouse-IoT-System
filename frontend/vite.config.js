9062d4fa4475a9c20d0de6c0c35c0446480da1e7
balintcristian <cristi.balint11@gmail.com>
2026-01-12 19:50:17 +0200

    yes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-      base: process.env.VITE_BASE_PATH || "/Greehouse-IoT-System/frontend",
+      base: "/",
     },
   },
 });
