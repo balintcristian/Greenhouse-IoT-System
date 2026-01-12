9d5976878bab83f0e782424b07826393c36eed3a
balintcristian <cristi.balint11@gmail.com>
2026-01-12 19:17:00 +0200

    built static webpage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
+      host: "192.168.0.38",
     },
   },
 });
